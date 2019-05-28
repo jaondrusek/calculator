da17d0c68df5b74d7bee942b0c005eae4ffdc1eb
Unknown <ondrusekjoshua@gmail.com>
2019-05-27 20:12:49 -0500

    Initial work on calculator
    Buttons structure set up, general dimensions for now picked.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CalcScreen />
+      <CalcButtons />
     </div>
   );
+}
+
+function CalcButtons() {
+  return (
+    <div className="buttons">
+      <button id="period">.</button>
+      <button id="zero">0</button>
+      <button id="one">1</button>
+      <button id="two">2</button>
+      <button id="three">3</button>
+      <button id="four">4</button>
+      <button id="five">5</button>
+      <button id="six">6</button>
+      <button id="seven">7</button>
+      <button id="eight">8</button>
+      <button id="nine">9</button>
+      <button id="add">+</button>
+      <button id="subtract">-</button>
+      <button id="multiply">x</button>
+      <button id="divide">/</button>
+      <button id="equals">=</button>
+      <button id="clear">AC</button>
+    </div>
+  );
+}
+
+class CalcScreen extends React.Component {
+  render() {
+    return (
+    <div id="screen">
+      <font>1432</font>
+    </div>
+    );
+  }
 }
 
 export default App;
