d63631dfdae232e43aa20ee0a9b0885bd146f6de
Unknown <ondrusekjoshua@gmail.com>
2019-06-01 13:51:46 -0500

    Working on adding calculator functionality
    Added event handlers for appending numbers. Handles starting null case. Correctly only adds one decimal. Starting mathematical operations.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,94 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <CalcScreen />
-      <CalcButtons />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expression: '',
+      value: '',
+      hasDecimal: false
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <ExpressionScreen expression={this.state.expression}/>
+        <ValueScreen value={this.state.value}/>
+        <CalcButtons 
+          appendNum={this.appendNum} 
+          addDecimal={this.addDecimal} 
+          clearValue={this.clearValue}
+          addNum={this.addNum}
+        />
+      </div>
+    );
+  }
+  addDecimal = (dec) => {
+    if (!this.state.hasDecimal) {
+        this.setState({
+          value: this.state.value + dec,
+          hasDecimal: true
+        })
+    }
+  }
+  appendNum = (num) => {
+    this.setState({
+      value: this.state.value + num
+    })
+  }
+  addNum = (exp) => {
+
+  }
+  clearValue = () => {
+    this.setState( {
+      value: ''
+    })
+  }
 }
 
-function CalcButtons() {
+function CalcButtons(props) {
   return (
     <div className="buttons">
-      <button className="num-button" id="period">.</button>
-      <button className="num-button" id="zero">0</button>
-      <button className="num-button" id="one">1</button>
-      <button className="num-button" id="two">2</button>
-      <button className="num-button" id="three">3</button>
-      <button className="num-button" id="four">4</button>
-      <button className="num-button" id="five">5</button>
-      <button className="num-button" id="six">6</button>
-      <button className="num-button" id="seven">7</button>
-      <button className="num-button" id="eight">8</button>
-      <button className="num-button" id="nine">9</button>
-      <button className="symbol-button" id="add">+</button>
+      <button className="num-button" id="period" value="." onClick={(e) => props.addDecimal(e.target.value)}>.</button>
+      <button className="num-button" id="zero" value="0" onClick={(e) => props.appendNum(e.target.value)}>0</button>
+      <button className="num-button" id="one" value="1" onClick={(e) => props.appendNum(e.target.value)}>1</button>
+      <button className="num-button" id="two" value="2" onClick={(e) => props.appendNum(e.target.value)}>2</button>
+      <button className="num-button" id="three" value="3" onClick={(e) => props.appendNum(e.target.value)}>3</button>
+      <button className="num-button" id="four" value="4" onClick={(e) => props.appendNum(e.target.value)}>4</button>
+      <button className="num-button" id="five" value="5" onClick={(e) => props.appendNum(e.target.value)}>5</button>
+      <button className="num-button" id="six" value="6" onClick={(e) => props.appendNum(e.target.value)}>6</button>
+      <button className="num-button" id="seven" value="7" onClick={(e) => props.appendNum(e.target.value)}>7</button>
+      <button className="num-button" id="eight" value="8" onClick={(e) => props.appendNum(e.target.value)}>8</button>
+      <button className="num-button" id="nine" value="9" onClick={(e) => props.appendNum(e.target.value)}>9</button>
+      <button className="symbol-button" id="add" value="+" onClick={(e) => props.addOpr(e.target.value)}>+</button>
       <button className="symbol-button" id="subtract">-</button>
       <button className="symbol-button" id="multiply">x</button>
       <button className="symbol-button" id="divide">/</button>
       <button className="equals-button" id="equals">=</button>
-      <button className="clear-button" id="clear">AC</button>
+      <button className="clear-button" id="clear" onClick={props.clearValue}>AC</button>
     </div>
   );
 }
 
-class CalcScreen extends React.Component {
-  render() {
+function ValueScreen(props) {
     return (
-    <div id="screen">
-      <font>14320</font>
-    </div>
+      <div id="val-screen">
+        <font>
+        {
+          props.value === '' ? '0' : props.value
+        }
+        </font>
+      </div>
     );
-  }
+}
+
+function ExpressionScreen(props) {
+    return (
+      <div id="exp-screen">
+        <font>{props.expression}</font>
+      </div>
+    );
 }
 
 export default App;
