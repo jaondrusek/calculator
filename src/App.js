2e0e25977147e958f4dce95f7d57f5ab2fac7093
Unknown <ondrusekjoshua@gmail.com>
2019-06-15 14:55:19 -0500

    Working version of simplified calculator
    Rewrote it as the previous version wasn't working and was getting messy.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import { create, all } from 'mathjs';
+
+const config = {};
+const math = create(all, config);
+
 function App() {
   return (
     <div className="App">
@@ -9,145 +14,127 @@ function App() {
   );
 }
 
-function add(x, y) {
-  return parseFloat(x) + parseFloat(y);
-}
-
-function subtract(x, y) {
-  return parseFloat(x) - parseFloat(y);
-}
-
-function multiply(x, y) {
-  return parseFloat(x) * parseFloat(y);
-}
-
-function divide(x, y) {
-  return parseFloat(x) / parseFloat(y);
-}
-
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       expression: '',
-      prevValue: 0,
-      currValue: 0,
-      realTotal: 0,
+      total: 0,
+      next: 0,
+      displayValue: 0,
       hasDecimal: false,
       operation: null,
-      operationJustAdded: false
+      operationJustAdded: false,
+      fresh: true
     }
   }
   render() {
     return (
       <div className="Calculator">
         <ExpressionScreen expression={this.state.expression}/>
-        <ValueScreen currValue={this.state.currValue}/>
+        <ValueScreen next={this.state.displayValue}/>
         <CalcButtons 
-          appendNum={this.appendNum} 
-          addDecimal={this.addDecimal} 
-          clearValue={this.clearValue}
-          addOperation={this.addOperation}
-          evaluate={this.evaluate}
-        />
+          handleKeyPress={this.handleKeyPress}/>
       </div>
     );
   }
-  addDecimal = (dec) => {
-    if (!this.state.hasDecimal) {
+  handleKeyPress = (e) => {
+    let numbers = ['0', '1', '2', '3', '4', '5', '5', '6', '7', '8', '9', '.'];
+    let operators = ['+', '-', '/', 'x'];
+    if(numbers.includes(e.target.name)){this.updateDisplay(e.target.name)}
+    if(operators.includes(e.target.name)){this.setOperator(e.target.name)}
+    if((e.target.name === 'AC')){this.allClear()}
+    if((e.target.name === '=')){this.executeExpression()}
+  }
+  updateDisplay = (val) => {
+    console.log('Update display');
+    if (val === '.' && !this.state.hasDecimal) {
       this.setState(function(prevState) {
-        let newValue =  prevState.currValue === 0 || prevState.operationJustAdded ? 0 + dec : prevState.currValue + dec;
+        let newValue = prevState.displayValue === 0 ? 0 + val : prevState.displayValue + val;
         return {
-          currValue: newValue,
-          operationJustAdded: false,
-          hasDecimal: true
+          displayValue: newValue,
+          hasDecimal: true,
+          operationJustAdded: false
         }
-      })
+      });
+    } else if (val !== '.') {
+      this.setState(function(prevState) {
+        let newValue = prevState.displayValue === 0 ? val : prevState.displayValue + val;
+        return {
+          displayValue: newValue,
+          operationJustAdded: false
+        }
+      });
     }
   }
-  appendNum = (num) => {
+  setOperator = (opr) => {
+    console.log('Set operator');
+    if (!this.state.operationJustAdded) {
+      this.setState(function(prevState) {
+        return {
+          expression: prevState.expression + ' ' + prevState.displayValue + ' ' + opr,
+          displayValue: 0,
+          operation: opr,
+          hasDecimal: false,
+          operationJustAdded: true
+        }
+      });
+    } else {
+      this.setState(function(prevState) {
+        return {
+          expression: prevState.expression.slice(0, -1) + opr,
+          operation: opr,
+        }
+      });
+    }
+  }
+  executeExpression = () => {
     this.setState(function(prevState) {
-      let newValue =  prevState.currValue === 0 || prevState.operationJustAdded ? num : prevState.currValue + num;
+      let full_expression = prevState.expression + ' ' + prevState.displayValue;
+      full_expression = full_expression.replace('x', '*');
+      console.log(full_expression);
+      let total = math.evaluate(full_expression);
       return {
-        currValue: newValue,
-        operationJustAdded: false
+        expression: '',
+        displayValue: total
       }
-    })
+    });
   }
-  addOperation = (opr, exp) => {
-    this.setState(function(prevState){
-      if (!prevState.operationJustAdded) {
-        console.log("currValue: " + prevState.currValue);
-        console.log("prevValue: " + prevState.prevValue);
-        return {
-          expression: prevState.expression + " " + prevState.currValue.toString() + ' ' + exp,
-          prevValue: prevState.currValue,
-          hasDecimal: false,
-          operation: opr,
-          operationJustAdded: true
-        }
-      } else if (prevState.operation !== opr) {
-          let symbol_index = prevState.expression.length - 1;
-          let fixed_exp = prevState.expression.substr(0, symbol_index) + exp;
-          console.log(typeof fixed_exp);
-        return {
-          expression: fixed_exp,
-          prevValue: prevState.currValue,
-          hasDecimal: false,
-          operation: opr,
-          operationJustAdded: true
-        }
-      }
-    })
-  }
-  clearValue = () => {
-    this.setState( {
-      currValue: 0,
-      prevValue: 0,
-      realTotal: 0,
+  allClear = () => {
+    console.log('Cleared');
+    this.setState({
       expression: '',
+      total: 0,
+      next: 0,
+      displayValue: 0,
       hasDecimal: false,
       operation: null,
-      operationJustAdded: false
-    })
-  }
-  evaluate = () => {
-    this.setState(function(prevState) {
-      if (prevState.operation !== null && prevState.operationJustAdded === false) {
-        let calculatedValue = prevState.operation(prevState.prevValue, prevState.currValue);
-        return {
-          currValue: calculatedValue,
-          prevValue: prevState.currValue,
-          expression: '',
-          hasDecimal: false
-        }
-      }
-    })
-    console.log("currValue: " + this.state.currValue);
-    console.log("prevValue: " + this.state.prevValue);
+      operationJustAdded: false,
+      fresh: true
+    });
   }
 }
 
 function CalcButtons(props) {
   return (
     <div className="buttons">
-      <button className="num-button" id="period" value="." onClick={(e) => props.addDecimal(e.target.value)}>.</button>
-      <button className="num-button" id="zero" value="0" onClick={(e) => props.appendNum(e.target.value)}>0</button>
-      <button className="num-button" id="one" value="1" onClick={(e) => props.appendNum(e.target.value)}>1</button>
-      <button className="num-button" id="two" value="2" onClick={(e) => props.appendNum(e.target.value)}>2</button>
-      <button className="num-button" id="three" value="3" onClick={(e) => props.appendNum(e.target.value)}>3</button>
-      <button className="num-button" id="four" value="4" onClick={(e) => props.appendNum(e.target.value)}>4</button>
-      <button className="num-button" id="five" value="5" onClick={(e) => props.appendNum(e.target.value)}>5</button>
-      <button className="num-button" id="six" value="6" onClick={(e) => props.appendNum(e.target.value)}>6</button>
-      <button className="num-button" id="seven" value="7" onClick={(e) => props.appendNum(e.target.value)}>7</button>
-      <button className="num-button" id="eight" value="8" onClick={(e) => props.appendNum(e.target.value)}>8</button>
-      <button className="num-button" id="nine" value="9" onClick={(e) => props.appendNum(e.target.value)}>9</button>
-      <button className="symbol-button" id="add" value="+" onClick={(e) => props.addOperation(add, e.target.value)}>+</button>
-      <button className="symbol-button" id="subtract" value="-" onClick={(e) => props.addOperation(subtract, e.target.value)}>-</button>
-      <button className="symbol-button" id="multiply" value="x" onClick={(e) => props.addOperation(multiply, e.target.value)}>x</button>
-      <button className="symbol-button" id="divide" value="\" onClick={(e) => props.addOperation(divide, e.target.value)}>/</button>
-      <button className="equals-button" id="equals" value="=" onClick={props.evaluate}>=</button>
-      <button className="clear-button" id="clear" onClick={props.clearValue}>AC</button>
+      <button className="num-button" id="period" name="." onClick={props.handleKeyPress}>.</button>
+      <button className="num-button" id="zero" name="0" onClick={props.handleKeyPress}>0</button>
+      <button className="num-button" id="one" name="1" onClick={props.handleKeyPress}>1</button>
+      <button className="num-button" id="two" name="2" onClick={props.handleKeyPress}>2</button>
+      <button className="num-button" id="three" name="3" onClick={props.handleKeyPress}>3</button>
+      <button className="num-button" id="four" name="4" onClick={props.handleKeyPress}>4</button>
+      <button className="num-button" id="five" name="5" onClick={props.handleKeyPress}>5</button>
+      <button className="num-button" id="six" name="6" onClick={props.handleKeyPress}>6</button>
+      <button className="num-button" id="seven" name="7" onClick={props.handleKeyPress}>7</button>
+      <button className="num-button" id="eight" name="8" onClick={props.handleKeyPress}>8</button>
+      <button className="num-button" id="nine" name="9" onClick={props.handleKeyPress}>9</button>
+      <button className="symbol-button" id="add" name="+" onClick={props.handleKeyPress}>+</button>
+      <button className="symbol-button" id="subtract" name="-" onClick={props.handleKeyPress}>-</button>
+      <button className="symbol-button" id="multiply" name="x" onClick={props.handleKeyPress}>x</button>
+      <button className="symbol-button" id="divide" name="/" onClick={props.handleKeyPress}>/</button>
+      <button className="equals-button" id="equals" name="=" onClick={props.handleKeyPress}>=</button>
+      <button className="clear-button" id="clear" name="AC" onClick={props.handleKeyPress}>AC</button>
     </div>
   );
 }
@@ -155,7 +142,7 @@ function CalcButtons(props) {
 function ValueScreen(props) {
     return (
       <div id="val-screen">
-        <font>{props.currValue}</font>
+        <font>{props.next}</font>
       </div>
     );
 }
